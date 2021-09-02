a5aca54e66c18f28ee44fccaa1a896ffc1419103
kevinNavarroNavarro <40080096+kevinNavarroNavarro@users.noreply.github.com>
2021-09-02 14:25:08 -0600

    Some changes

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,11 @@ RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
 ENTRYPOINT ["gunicorn", "-b", ":8080", "main:APP"]
+
+
+
+export URL="a403fa53a96514f12948dd65f87550c1-1498351496.us-east-2.elb.amazonaws.com"
+
+export TOKEN=`curl -d '{"email":"test@test.com","password":"test"}' -H "Content-Type: application/json" -X POST $URL/auth  | jq -r '.token'`
+
+curl --request GET $URL:80/contents -H "Authorization: Bearer ${TOKEN}" | jq
--- test_main.py
+++ test_main.py
@@ -20,12 +20,15 @@ def client():
 
     yield client
 
-
-
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
+
+def fail_test_health(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json == 'Bad Request'
 
 
 def test_auth(client):
@@ -36,5 +39,16 @@ def test_auth(client):
                            content_type='application/json')
 
     assert response.status_code == 200
+    token = response.json['token']
+    assert token is not None
+
+def test_auth(client):
+    body = {'email': EMAIL,
+            'password': PASSWORD}
+    response = client.post('/auth', 
+                           data=json.dumps(body),
+                           content_type='application/json')
+
+    assert response.status_code == 401
     token = response.json['token']
     assert token is not None
